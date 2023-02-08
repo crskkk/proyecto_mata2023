af6f8bee08426afa4b8cbdfd68bced8fcbc26943
Cris <duqueorange+github@gmail.com>
2023-02-07 21:04:00 -0500

    Big NPM Fuckup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Auth from "./components/Auth";
+import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import Modal from "./components/Modal";
+import ProgressBar from "./components/ProgressBar";
+import TickIcon from "./components/TickIcon";
+
+const App = () => {
+  return (
+    <div className="app">
+      <ListHeader listName = {'Tick List'} />
+    </div>
+  );
+}
+
+export default App;
